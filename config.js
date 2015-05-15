3c5a8e5bc11d71b10d1056a2b390af07ae43bb45
brandonweis <kokweiong@gmail.com>
2015-05-16 00:42:49 +0800

    restructure

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 var config = {
   "production":{
-      "apiHost": "http://dashboardapi.cirqle.nl"
+      "apiHost": "http://dashboardapi.cirqle.nl",
+      "cdnHost": "http://cdn.cirqle.nl"
   },
   "staging":{
-      "apiHost": "http://dashboardapi-staging.cirqle.nl"
+      "apiHost": "http://dashboardapi-staging.cirqle.nl",
+      "cdnHost": "http://cdn.cirqle.nl"
   },
   "development":{
-      "apiHost": "http://localhost:9000"
+      "apiHost": "http://localhost:9000",
+      "cdnHost": "http://localhost:8888"
   }
 }
 
