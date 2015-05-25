3cf5536324e274e2b7dee3a5a02d0f54713179a7
brandonweis <kokweiong@gmail.com>
2015-05-25 18:07:40 +0800

    remove test for shopbox

--- test/tests/wordpress.js
+++ test/tests/wordpress.js
@@ -10,10 +10,10 @@ module.exports = {
       .waitForElementVisible('div[id=cq-shopwindow]', 10000)
       .pause(3000)
 
-      .waitForElementVisible('a[id=shopboxButton]', 12000)
-      .click('a[id=shopboxButton]')
-      .pause(900)
-      .waitForElementVisible('div[id=cq-shopwindow]', 10000)
+      // .waitForElementVisible('a[id=shopboxButton]', 12000)
+      // .click('a[id=shopboxButton]')
+      // .pause(900)
+      // .waitForElementVisible('div[id=cq-shopwindow]', 10000)
       .end();
   }
 };
