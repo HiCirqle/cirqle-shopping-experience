477126dcf5acdccbddad95d2352137c3e6939c74
brandonweis <kokweiong@gmail.com>
2015-06-22 22:45:59 +0800

    handle exponential number in zIndex

--- btn/modules/highestZIndex.js
+++ btn/modules/highestZIndex.js
@@ -6,22 +6,28 @@ function highestZIndex(defaultIndex, btn){
   if(!highest){highest = 8;} // becasue blogger image slider is 10
 
   highest = Math.max.apply(null,cqjq.map(cqjq('body *'), function(e,n){
-      var current = parseInt(cqjq(e).css("z-index"), 10);
+      var current = cqjq(e).css("z-index");
+      current = parseInt(Number(current), 10);
+
       var position = cqjq(e).css("position");
       var isVisible = cqjq(e).is(":visible");
       var className = cqjq(e).attr("class");
       var id = cqjq(e).attr("id");
       // console.log(cqjq(e));
       if(current && !isNaN(current) && position && position != "static" && isVisible && className != "cirqle-outer-button" && id != "cq-shopwindow"){
-          var intersect = findIntersector(e, btn);
-          if(intersect.length > 0){
-              // console.log("highestZIndex",current);
-              return current;
-          }
-          else{
-              // console.log("highestZIndex",defaultIndex);
-              return defaultIndex;
-          }
+        if (!btn) {
+          return current;
+        }
+
+        var intersect = findIntersector(e, btn);
+        if(intersect.length > 0){
+            // console.log("highestZIndex",current);
+            return current;
+        }
+        else{
+            // console.log("highestZIndex",defaultIndex);
+            return defaultIndex;
+        }
       }else{
           // console.log("highestZIndex",defaultIndex);
           return defaultIndex;
